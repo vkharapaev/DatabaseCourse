f4c73057d390762cd424d393b25638f1f0fcd16e
Vladimir Kharapaev <vkharapaev@gmail.com>
2020-11-18 01:42:37 +0500

    hw7

---
+++ hw7/hw7.sql
@@ -0,0 +1,12 @@
+-- задание 1
+create user shop_read;
+grant select on shop.* to shop_read;
+create user shop;
+grant all on shop.* to shop;
+
+-- задание 2
+create table accounts (id serial primary key, name varchar(255), password varchar(255));
+insert into accounts values (null, 'person1', 'pass1'), (null, 'person2', 'pass2'), (null, 'person3', 'pass3');
+create view username as select id, name from accounts;
+create user shop_read;
+grant select on shop.username to shop_read;
---
+++ hw7/hw7_extended.sql
@@ -0,0 +1,17 @@
+-- задание 1
+drop user if exists shop_read;
+create user shop_read;
+grant select on shop.* to shop_read;
+drop user if exists shop;
+create user shop;
+grant all on shop.* to shop;
+
+-- задание 2
+drop table if exists accounts;
+create table accounts (id serial primary key, name varchar(255), password varchar(255));
+insert into accounts values (null, 'person1', 'pass1'), (null, 'person2', 'pass2'), (null, 'person3', 'pass3');
+drop view if exists username;
+create view username as select id, name from accounts;
+drop user if exists shop_read;
+create user shop_read;
+grant select on shop.username to shop_read;
